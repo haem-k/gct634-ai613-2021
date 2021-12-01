3261d5e048bbfdeedb1e166fceb080b5cd6c8c02
Haemin <spot1223@kaist.ac.kr>
2021-12-01 15:13:45 +0900

    Split all 21 songs from STV bgm

--- hw4/split_wav.py
+++ hw4/split_wav.py
@@ -1,11 +1,13 @@
 import wave
 
 # times between which to extract the wave from
-start = [208.0, 288.0, 458.7, 599.5, 778.8, 869.8, 992.0, 1072, 1200, 1276, 1384, 1462, 1504, 1605, 1772, 1952, 2066, 2183, 2316, 2396.9] # seconds
-end = [286.0, 458.7, 599.0, 778.7, 869.7, 980.0, 1072, 1196.0, 1276, 1384, 1461.1, 1504, 1604.8, 1760, 1952, 2065, 2182.6, 2315.3, 2396.7, 2518] # seconds
+start = [0, 208.0, 288.0, 458.7, 599.5, 778.8, 869.8, 992.0, 1072, 1200, 1276, 1384, 1462, 1504, 1605, 1772, 1952, 2066, 2183, 2316, 2396.9] # seconds
+end = [208.0, 286.0, 458.7, 599.0, 778.7, 869.7, 980.0, 1072, 1196.0, 1276, 1384, 1461.1, 1504, 1604.8, 1760, 1952, 2065, 2182.6, 2315.3, 2396.7, 2518] # seconds
+assert len(start) == len(end)
 
-songs = 20
-song_names = ['grandpa', 'spring1', 'spring2', 'spring3', 'pelican', 'library', 'saloon', 'summer1', 'summer2', 'summer3', 'guild', 'mines', 'carpenter', 'dance', 'fall1', 'fall2', 'fall3', 'winter1', 'winter2', 'winter3']
+
+songs = 21
+song_names = ['opening','grandpa', 'spring1', 'spring2', 'spring3', 'pelican', 'library', 'saloon', 'summer1', 'summer2', 'summer3', 'guild', 'mines', 'carpenter', 'dance', 'fall1', 'fall2', 'fall3', 'winter1', 'winter2', 'winter3']
 
 for i in range(songs):
     # file to extract the snippet from
